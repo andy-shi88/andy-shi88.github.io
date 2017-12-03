2beb96f584815013ccbb9b5d33da850ad3ec6abb
andy-shi88 <shi88.andy@gmail.com>
2017-12-03 11:27:52 +0700

    rough update

--- contents/main.php
+++ contents/main.php
@@ -4,7 +4,66 @@
             Welcome to My Github Portfolios Page
         </h1>
     </div>
-    
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-check"></i>Indonesia Developer Summit 2017 Backend</h4>
+                <h6 id="programming-language">Python Flask, Jinja2 template, javascript, html, css</h6>
+            </div>
+            <div class="panel-body">
+                <p>Lead the backend development of Indonesia Developer Summit. Consist of ticket ordering, promotion system, beacon integration, exhibitor management, mailing system, admin management, social feed system and other features. Also in charge of managing the server, such as deployment, docker compose setup, etc.</p>
+                <a href="#" class="btn btn-default">Private</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-check"></i>Front End Keller William CRM</h4>
+                <h6 id="programming-language">React JS, Javascript</h6>
+            </div>
+            <div class="panel-body">
+                <p>Work on KW CRM frontend development using React JS.</p>
+                <a href="#" class="btn btn-default">Private</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-check"></i>Mobile Keller William - Kelle Assistant</h4>
+                <h6 id="programming-language">React native, Javascript</h6>
+            </div>
+            <div class="panel-body">
+                <p>Kelle mobile AI apps for assisting agent, work on providing UI for chatting environment for interacting with kelle the ai assistant.</p>
+                <a href="#" class="btn btn-default">Private</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-check"></i>Mobile Hiekr</h4>
+                <h6 id="programming-language">Java, android, xml</h6>
+            </div>
+            <div class="panel-body">
+                <p>Social apps for hiker, built in java android. worked on various feature such as social sign in, feed system, following system, commenting, file upload, location, sharing apps, and offline handling. Used some android common used libraries such as dependency injection using dagger, gson, retrofit, eventbus, butterknife, etc.</p>
+                <a href="#" class="btn btn-default">Private</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><i class="fa fa-fw fa-check"></i>Keller William CRM - Backend</h4>
+                <h6 id="programming-language">React JS, Javascript</h6>
+            </div>
+            <div class="panel-body">
+                <p>Work KW Microservices development. Participated on contact micros, core-package micros, and mailchimp micros. Where each have different approach such as providing the core functionalities on core-package, contact related feature on contact micros and maily proxying on mailchimp micros. Jobs include writing both functionalities, apiary specs and unit testing.</p>
+                <a href="#" class="btn btn-default">Private</a>
+            </div>
+        </div>
+    </div>
     <div class="col-md-4">
         <div class="panel panel-default">
             <div class="panel-heading">
