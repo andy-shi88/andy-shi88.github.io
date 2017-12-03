fdb8ec64a0c2ba16a882cebd61245a7f64dd5853
andy-shi88 <shi88.andy@gmail.com>
2017-12-03 11:48:48 +0700

    row fix

--- route/route.php
+++ route/route.php
@@ -3,9 +3,6 @@
        case 'main':
        		include './contents/main.php';
        		break;
-       case 'manajemen_stok':
-            include './content/Forms/form_manajemen_stok.php';
-            break;
     default:
       include './contents/main.php';
   		break;
